0d7c477ad0f06dd2453cd70328a2530e1c228fe2
aishwarya-chandra <aish3ch@gmail.com>
2025-04-20 15:15:43 +0530

    working of react

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));              //React creates it's own virtual DOM and root of that is div with id root of index.html file
+root.render(                                                                    //then all the components are rendered there
   <React.StrictMode>
-    <App />
+    <App />                                                                    {/* here App named component is getting rendered */}
   </React.StrictMode>
 );
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(                      //React creates it's own virtual DOM and root of that is div with id root of index.html file
   <StrictMode>
     <App />
   </StrictMode>,
