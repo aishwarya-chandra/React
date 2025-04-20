89e3040ea5478a558aaf67d48db0846653efb282
aishwarya-chandra <aish3ch@gmail.com>
2025-04-20 14:58:10 +0530

    file creation in vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
+import Demo from './Demo.jsx'
 
 function App() {
 
   return (
-    <h1>onto react journey with vite</h1>
+    <>                      {/*(<>__</>) these are called fragments, used because jsx returns only single element, which inside can have multiple components */}
+    <Demo />                {/*the demo file is called */}
+      <h1>onto react journey with vite</h1>
+    </>
   )
 }
 
---
+++ 01vitereact/src/Demo.jsx
@@ -0,0 +1,8 @@
+
+function Demo() {                           //function name in capital is necessary and file should be .jsx
+    return (
+        <h3>This is a demo statement.</h3>
+    )
+}
+
+export default Demo
